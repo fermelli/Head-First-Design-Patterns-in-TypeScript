d4a99231a3b5c392bc46132b57b0a05271267da6
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-08-05 14:38:35 -0400

    arreglado llamada a clases correctas

--- src/01-the-strategy-pattern/game-adventure/main.ts
+++ src/01-the-strategy-pattern/game-adventure/main.ts
@@ -1,7 +1,7 @@
 import { Character } from "./class-character";
-import { King } from "./classes-characters";
+import { King, Knight, Queen, Troll } from "./classes-characters";
 
 export const theKing: Character = new King();
-export const theQueen: Character = new King();
-export const theKnight: Character = new King();
-export const theTroll: Character = new King();
+export const theQueen: Character = new Queen();
+export const theKnight: Character = new Knight();
+export const theTroll: Character = new Troll();
